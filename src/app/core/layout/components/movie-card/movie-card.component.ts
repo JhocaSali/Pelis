12780af6b2de08581a6a86f0da4998c5348c5bf5
JhocaSali <jcsalinas@ucompensar.edu.co>
2026-05-03 19:33:18 -0500

    Se crean las tarjetas de peliculas

--- src/app/core/layout/components/movie-card/movie-card.component.ts
+++ src/app/core/layout/components/movie-card/movie-card.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [],
+  standalone: true,
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.css'
 })
 export class MovieCardComponent {
+  @Input() title: string = '';
+  @Input() imageUrl: string = '';
+  @Output() resumenClicked = new EventEmitter<void>();
 
+  onResumenClick() {
+    this.resumenClicked.emit();
+    }
 }
--- src/app/core/layout/layout-shell.component.ts
+++ src/app/core/layout/layout-shell.component.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { AppFooterComponent } from './components/app-footer/app-footer.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
 
 @Component({
   selector: 'app-layout-shell',
   standalone: true,
-  imports: [RouterOutlet, AppHeaderComponent, AppFooterComponent],
+  imports: [CommonModule, RouterOutlet, AppHeaderComponent, AppFooterComponent, MovieCardComponent],
   template: `
     <div class="app-shell">
       <app-header />
 
       <main class="app-main">
+        <div class="movie-grid">
+          <app-movie-card 
+            title="Michael" 
+            imageUrl="img/tendencia1.jpg" />
+
+          <app-movie-card 
+            title="Apex" 
+            imageUrl="img/tendencia2.jpg" />
+
+          <app-movie-card 
+            title="Project Hail Mary" 
+            imageUrl="img/tendencia3.jpg" />
+        </div>
+
         <router-outlet />
       </main>
-
+      
       <app-footer />
     </div>
   `,
@@ -23,14 +39,22 @@ import { AppFooterComponent } from './components/app-footer/app-footer.component
       min-height: 100vh;
       background-color: #0a0a0a;
       color: #ffffff;
-      display: flex;    
+      display: flex;
       flex-direction: column;
     }
 
     .app-main {
       flex: 1;
       width: 100%;
+      padding: 20px;
     }
-  `],
+
+    .movie-grid {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 20px;
+    }
+  `]
 })
 export class LayoutShellComponent {}
